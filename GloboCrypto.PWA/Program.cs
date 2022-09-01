41c6139eaf847519cd70b77f6781edd1162de534
MatteoRigoni <matteo.rigoni2@gmail.com>
2022-09-01 11:48:56 +0200

    added azure logging

--- GloboCrypto.PWA/Program.cs
+++ GloboCrypto.PWA/Program.cs
@@ -4,10 +4,15 @@ using GloboCrypto.PWA;
 using GloboCrypto.PWA.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.AzureAppServices;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.Configure<AzureFileLoggerOptions>(builder.Configuration.GetSection("AzureLogging"));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ GloboCrypto.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using GloboCrypto.Model.Authentication;
+using GloboCrypto.WebAPI.Services.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboCrypto.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticationService AuthenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            AuthenticationService = authenticationService;
+        }
+
+        [HttpGet("authenticate")]
+        [Authorize(Policy = "ApiKeyPolicy")]
+        public async Task<AuthTokenResponse> Authenticate(string id)
+        {
+            return await AuthenticationService.Authenticate(id);
+        }
+    }
+}
